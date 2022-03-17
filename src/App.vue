8797dbbaecbcbdbd4002b4d9a2dbfd9550288452
akrulcic <akrulcic@users.noreply.github.com>
2022-03-17 11:44:08 +0100

    promjena na navbar i sidebar

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,18 @@
 <template>
 
 
-  <nav id="nav" class="navbar navbar-expand-md navbar-light">
+  <nav id="nav" class="navbar navbar-light">
     <router-link to="/" class="navbar-brand">
-                <img src="@/assets/logo.png" height="50" class="d-inline-block align-top" alt="Logo" />
-            </router-link>
+      <img src="@/assets/logo.png" height="50" class="d-inline-block align-top" alt="Logo" />
+    </router-link>
+
+    <form id="search" class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="traži" aria-label="Search">
+    </form>
+
+    <router-link to="" class="navbar-brand">
+      <img id="user" src="@/assets/user.png" height="35" class="d-inline-block" alt="user" />
+    </router-link>
             
     <!--<router-link to="/">Home</router-link> |
     <router-link to="/login">Login</router-link>
@@ -14,65 +22,7 @@
   <br/>
   <br/>
 
-  <div class="row">
-    <div class="col-4">
-      <nav class="navbar navbar-expand-lg navbar-light mb-2">
-      <div class="sidebar">
-        
-        <ul class="nav_list">
-          <li>
-            <a href="#">
-              <img src="@/assets/pin.png" height="20"  />
-              <span class="links_name">Najpopularnije</span>
-            </a>
-          </li>
-          <br/>
-          <br/>
-          <li>
-            <a href="#">
-              <img src="@/assets/pin.png" height="20"  />
-              <span class="links_name">Novo</span>
-            </a>
-          </li>
-          <br/>
-          <br/>
-          <li>
-            <a href="#">
-              <img src="@/assets/pin.png" height="20"  />
-              <span class="links_name">Težina staze:</span>
-            </a>
-          </li>
-          <br/>
-          <br/>
-          <li>
-            <a href="#">
-              <img src="@/assets/pin.png" height="20"  />
-              <span class="links_name">Najbolje ocjenjeno</span>
-            </a>
-          </li>
-          <br/>
-          <br/>
-          <li>
-            <a href="#">
-              <img src="@/assets/pin.png" height="20"  />
-              <span class="links_name">Duljina staze</span>
-            </a>
-          </li>
-          <br/>
-          <br/>
-          <li>
-            <a href="#">
-              <img src="@/assets/pin.png" height="20"  />
-              <span class="links_name">Želim posjetiti</span>
-            </a>
-          </li>
-
-        </ul>
-      </div>
-      </nav>
-    </div>
-    <div class="col-8"></div>
-  </div>
+  
 
   <router-view/>
 </template>
@@ -89,10 +39,8 @@ nav {
       color: #42b983;
     }
   }
+
 }
-.nav_list{
-    list-style:none;
-    text-align: left;
-    padding-left: 30px;
-}
+
+
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,87 @@
 <template>
+        <div class="row">
+          <div class="col-4">
+            <nav class="navbar navbar-expand-lg navbar-light mb-2">
+            <div class="sidebar">
+              
+              <ul class="nav_list">
+                <li>
+                  <a href="#">
+                    <img src="@/assets/pin.png" height="20"  />
+                    <span class="links_name">Najpopularnije</span>
+                  </a>
+                </li>
+                <br/>
+                <br/>
+                <li>
+                  <a href="#">
+                    <img src="@/assets/pin.png" height="20"  />
+                    <span class="links_name">Novo</span>
+                  </a>
+                </li>
+                <br/>
+                <br/>
+                <li>
+                  <a href="#">
+                    <img src="@/assets/pin.png" height="20"  />
+                    <span class="links_name">Težina staze:</span>
+                  </a>
+                </li>
+                <br/>
+                <br/>
+                <li>
+                  <a href="#">
+                    <img src="@/assets/pin.png" height="20"  />
+                    <span class="links_name">Najbolje ocjenjeno</span>
+                  </a>
+                </li>
+                <br/>
+                <br/>
+                <li>
+                  <a href="#">
+                    <img src="@/assets/pin.png" height="20"  />
+                    <span class="links_name">Duljina staze</span>
+                  </a>
+                </li>
+                <br/>
+                <br/>
+                <li>
+                  <a href="#">
+                    <img src="@/assets/pin.png" height="20"  />
+                    <span class="links_name">Želim posjetiti</span>
+                  </a>
+                </li>
+
+              </ul>
+            </div>
+            </nav>
+          </div>
+
+          <div class="col-8">
+              <card/>
+
+          </div>
+    </div>
+
   
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import card from '@/components/card.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    card
   }
 }
 </script>
+
+<style lang="scss">
+.nav_list{
+    list-style:none;
+    text-align: left;
+    padding-left: 30px;
+}
+</style>
